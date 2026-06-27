b2b95524ca8d61d1d766715549950cf412e38ba1
Sam Shahzad <78374032+Sam5888@users.noreply.github.com>
2026-06-27 09:00:17 -0400

    Add interactive tutorial and gameplay objectives

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,6 +12,7 @@ void main() {
     expect(find.text('TAG TAG'), findsOneWidget);
     expect(find.text('STORE'), findsOneWidget);
     expect(find.text('SETUP'), findsOneWidget);
+    expect(find.text('LEARN TO PLAY'), findsOneWidget);
 
     await tester.ensureVisible(find.text('PLAY'));
     await tester.pump();
@@ -22,5 +23,19 @@ void main() {
     expect(find.text('TAGS'), findsOneWidget);
     expect(find.text('DASH'), findsOneWidget);
     expect(find.byIcon(Icons.timer_outlined), findsOneWidget);
+  });
+
+  testWidgets('home practice launches interactive tutorial', (tester) async {
+    SharedPreferences.setMockInitialValues({});
+    await tester.pumpWidget(const TagTagApp());
+    await tester.pump();
+
+    await tester.tap(find.text('LEARN TO PLAY'));
+    await tester.pump();
+
+    expect(find.text('INTERACTIVE TUTORIAL'), findsOneWidget);
+    expect(find.text('Move To Survive'), findsOneWidget);
+    expect(find.text('DRAG'), findsOneWidget);
+    expect(find.text('EXIT'), findsOneWidget);
   });
 }
